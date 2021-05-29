b2e6319bf566a1d33f36bf758acd0ea25ae10859
Mohammad Nasirifar <farnasirim@gmail.com>
2021-05-29 10:38:07 -0400

    Add is prime 98

---
+++ is-prime-98.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+
+// IsPrime::value = true if v is prime, flase otherwise.
+template<int v>
+struct IsPrime;
+
+// HasDivisorGE::value = true if v is divisible by d or any number greater
+// than d.
+template<int v, int d>
+struct HasDivisorGE {
+  static constexpr bool value = (d < v) &&
+    ((v % d == 0) || HasDivisorGE<v, d + 1>::value);
+};
+
+template<int v>
+struct HasDivisorGE<v, v> {
+  static const bool value = false;
+};
+
+template<int v>
+struct IsPrime {
+  static const bool value = !HasDivisorGE<v, 2>::value;
+};
+
+int main() {
+  std::cout << 7 << " : " << IsPrime<7>::value << std::endl;
+
+  // fatal error: template instantiation depth exceeds maximum of 900
+  // (use ‘-ftemplate-depth=’ to increase the maximum)
+  // std::cout << 2000 << " : " << IsPrime<2000>::value << std::endl;
+  return 0;
+}
