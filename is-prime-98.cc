2cb0f18cb289e803d5acb877bb0a3366d56dedbc
Mohammad Nasirifar <farnasirim@gmail.com>
2021-05-29 12:29:42 -0400

    Add docs and cleanup recursion in 98

--- is-prime-98.cc
+++ is-prime-98.cc
@@ -1,15 +1,10 @@
 #include <iostream>
 
-// IsPrime::value = true if v is prime, flase otherwise.
-template<int v>
-struct IsPrime;
-
 // HasDivisorGE::value = true if v is divisible by d or any number greater
-// than d.
+// than or equal to d.
 template<int v, int d>
 struct HasDivisorGE {
-  static const bool value = (d < v) &&
-    ((v % d == 0) || HasDivisorGE<v, d + 1>::value);
+  static const bool value = (v % d == 0) || HasDivisorGE<v, d + 1>::value;
 };
 
 template<int v>
@@ -17,6 +12,7 @@ struct HasDivisorGE<v, v> {
   static const bool value = false;
 };
 
+// IsPrime::value = true if v is prime, flase otherwise.
 template<int v>
 struct IsPrime {
   static const bool value = !HasDivisorGE<v, 2>::value;
