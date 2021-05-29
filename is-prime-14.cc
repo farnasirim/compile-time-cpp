f244aeff745423e636fef753b58284c63f30e9ef
Mohammad Nasirifar <farnasirim@gmail.com>
2021-05-29 13:59:42 -0400

    Update 14

--- is-prime-14.cc
+++ is-prime-14.cc
@@ -1,8 +1,5 @@
 #include <iostream>
 
-template<int v>
-struct IsPrime;
-
 template<int v, int d, typename = std::enable_if_t<(d <= v)>>
 struct HasDivisorGE {
   static const bool value = (d < v) &&
@@ -15,18 +12,18 @@ struct IsPrime {
 };
 
 int main() {
-  // compile-time-cpp/is-prime-14.cc: In instantiation of ‘const bool HasDivisorGE<7, 7, void>::value’:
-  // compile-time-cpp/is-prime-14.cc:9:46:   recursively required from ‘const bool HasDivisorGE<7, 3, void>::value’
-  // compile-time-cpp/is-prime-14.cc:9:46:   required from ‘const bool HasDivisorGE<7, 2, void>::value’
-  // compile-time-cpp/is-prime-14.cc:14:50:   required from ‘const bool IsPrime<7>::value’
-  // compile-time-cpp/is-prime-14.cc:18:42:   required from here
-  // compile-time-cpp/is-prime-14.cc:9:46: error: no type named ‘type’ in ‘struct std::enable_if<false, void>’
-  //     9 |     ((v % d == 0) || HasDivisorGE<v, d + 1>::value);
+  // In instantiation of ‘const bool HasDivisorGE<7, 7, void>::value’:
+  //   recursively required from ‘const bool HasDivisorGE<7, 3, void>::value’
+  //   required from ‘const bool HasDivisorGE<7, 2, void>::value’
+  //   required from ‘const bool IsPrime<7>::value’
+  //   required from here
+  // error: no type named ‘type’ in ‘struct std::enable_if<false, void>’
+  //       |     ((v % d == 0) || HasDivisorGE<v, d + 1>::value);
   //                                                      ^~~~~
   std::cout << 7 << " : " << IsPrime<7>::value << std::endl;
 
   // fatal error: template instantiation depth exceeds maximum of 900
   // (use ‘-ftemplate-depth=’ to increase the maximum)
-  // std::cout << 2000 << " : " << IsPrime<2000>::value << std::endl;
+  std::cout << 2000 << " : " << IsPrime<2000>::value << std::endl;
   return 0;
 }
